c780fb5aa5a705210c7b5967752f522e0f7ba8ff
hothaifa zoubi <hothaifazoubi@gmail.com>
2024-12-06 12:20:21 +0200

    add prometheus first

--- prometheus-1/prometheus-exporter/app.py
+++ prometheus-1/prometheus-exporter/app.py
@@ -1,37 +1,29 @@
 import random
+import time
 from flask import Flask
 from prometheus_client import start_http_server,Gauge,Summary,Counter,Histogram,generate_latest
 
 app = Flask(__name__) 
 
-HTTP_REQUESTS=Counter('HTTP_REQUESTS','count of total http')
-AUth_users=Counter('Authenticated_users','count of total http')
-HTTP_REQUEST_LATENCY=Histogram('HTTP_LAT','histogram to show the request lat')
+HTTP_REQUESTS=Counter('http_request_total','count of total http',['endpoint','method','code'])
+AUTH_USER=Counter('Authenticated_users_requests_total','count of total http')
+HTTP_REQUEST_LATENCY=Histogram('http_request_duration_seconds','histogram to show the request lat',['endpoint','method'])
 
-
-@app.before_request
-def before_request():
-    AUth_users.inc(4)
-    HTTP_REQUESTS.inc(1)
-    # HTTP_REQUEST_LATENCY.observe(5.4,[])
-
-@app.after_request
-def after_request(response):
-    return response
 
 @app.get('/metrics')
 def metrics():
-    return generate_latest(),200
+    start_time= time.time()
+    HTTP_REQUESTS.labels(endpoint='/metrics',method='get',code=200).inc(1)
+    duration=time.time() - start_time
+    HTTP_REQUEST_LATENCY.labels(endpoint='/metrics',method='get').observe(duration)
+    return generate_latest()
+@app.get('/login')
+def login():
+    if random.randint(0,100) > 50:
+        HTTP_REQUESTS.labels(endpoint='/login',method='get',code='200').inc(1)
+        return '200'
+    else:
+        HTTP_REQUESTS.labels(endpoint='/login',method='get',code='400').inc(1)
+        return '400'
 
-@app.post('/hello') 
-def gett_hello():
-     
-    return 'world !'
-
-@app.get('/math')
-def get_math():
-    x = random.randrange(1,100)
-    y = random.randrange(1,100)
-    return f'{x} + {y}  = ?'
-
-app.run(port=8881)
+app.run(port=5001)
